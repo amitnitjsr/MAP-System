5fd0b0ea9f33a8d7aa427525e9d906fd9e834547
amitnitjsr <amitnips007@gmail.com>
2020-07-25 17:54:47 +0530

    first commit

---
+++ src/component/Home/Home.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react'
+
+class Home extends Component {
+    state = {
+        someobject: {
+            CountryCode: [{ 91: 'India' }, { 61: 'Australia' }],
+            States: {
+                India: ["Karnataka", "Delhi", "Maharashtra", "TamilNadu"],
+                Australia: ["Queensland", "Tasmania", "Victoria"]
+            }
+        }
+        ,
+        someobject1: {
+            India: ["TamiNadu", "Andra pradesh", "Bihar", "Maharashtra"],
+            Australia: ["Westren Australia"]
+        },
+        countryCode: null,
+    }
+    inputHandler = (value) => {
+        this.setState({ countryCode: value }, () => {
+            let tmp = this.state.someobject.CountryCode.filter((val) => val === this.state.countryCode)
+            // this.state.someobject.CountryCode.map((val) => {
+            //     console.log(Object.entries(val[0]))
+            // })
+            console.log('tmp', tmp, this.state.someobject)
+        })
+    }
+    render() {
+        return (
+            <div>
+                <h1>Task 1</h1>
+                <label>Enter Country Code:</label>
+                <input value={this.state.countryCode} onChange={(event) => this.inputHandler(event.target.value)} />
+            </div>
+        )
+    }
+}
+
+export default Home;
